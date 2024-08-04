0ad17937553d5c0a69360165c1c449a1afafae46
Sheng Yu Chang <shengyuchang@Shengs-Air.lan>
2024-08-04 01:27:28 -0700

    added images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "s3-inven-management-bucket.s3.us-west-1.amazonaws.com",
+                port: "",
+                pathname: "/**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -12,6 +12,7 @@ import {
     SlidersHorizontal,
     User,
 } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -74,7 +75,13 @@ const Sidebar = () => {
                     isSidebarCollapsed ? "px-5" : "px-8"
                 }`}
             >
-                <div>logo</div>
+                <Image
+                    src="https://s3-inven-management-bucket.s3.us-west-1.amazonaws.com/logo.png"
+                    alt="logo"
+                    width={27}
+                    height={27}
+                    className="rounded w-8"
+                />
                 <h1
                     className={`${
                         isSidebarCollapsed ? "hidden" : "block"
